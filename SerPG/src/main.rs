f06efbc363d4a6338c385e3b612c4adc640c5b38
bcedu <bcedu>
2026-02-28 12:27:27 +0100

    Crear path si no existeix

--- SerPG/src/main.rs
+++ SerPG/src/main.rs
@@ -65,6 +65,9 @@ impl SerPG {
     async fn get_partides_guardades(State(spg_state): State<SerPGState>, Path(videojoc_id): Path<String>) -> Json<Vec<PartidaGuardadaAPI>> {
         let mut partides_list = Vec::new();
         let videojoc_path = format!("{}/{}", spg_state.videojocs_path, videojoc_id);
+        if !PathBuf::from(&videojoc_path).exists() {
+            fs::create_dir(videojoc_path.clone()).unwrap();
+        }
         for entry in fs::read_dir(videojoc_path).unwrap() {
             let entry = entry.unwrap();
             let full_path = entry.path();
@@ -89,8 +92,13 @@ async fn main() {
 pub mod tests {
     use crate::SerPG;
     use urlencoding::encode;
-    use std::path::PathBuf;
+    use std::path::{PathBuf, Path};
+    use tokio::fs::remove_dir;
     async fn setup_server() {
+        // Fem neteja decoses que no haurien de existir
+        let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/pastanaga bullida").to_str().unwrap().to_string();
+        remove_dir(&test_path).await;
+        // Fem el servidor
         let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures").to_str().unwrap().to_string();
         tokio::spawn(async {
             SerPG::new(test_path).start(Some("3001".to_string())).await;
@@ -129,8 +137,16 @@ pub mod tests {
     #[tokio::test]
     async fn test_api_get_partides_guardades() {
         let server = setup_server().await;
+        // Joc que no existeix
+        let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/pastanaga bullida").to_str().unwrap().to_string();
+        let res = make_get_request("videojocs/pastanaga bullida/partides").await;
+        assert_eq!(res, "[]");
+        let path = Path::new(&test_path);
+        assert!(Path::new(&path).exists());
+        // Joc sense partides
         let res = make_get_request("videojocs/Warhammer 50k/partides").await;
         assert_eq!(res, "[]");
+        // Joc amb partides
         let res = make_get_request("videojocs/Napoleón TW HD/partides").await;
         let expected_res = "[{\"nom\":\"save1.txt\",\"hash\":\"02d47a22e09f46731a58dbe7cb299c0315c6760aec7557e8ca6e87090fc85dfd\"},{\"nom\":\"save3.txt\",\"hash\":\"158ed8c255d81393d423bc01c4993eceb3bb20a2596659ebc7f14ae82cbde4c8\"}]";
         assert_eq!(res, expected_res);
