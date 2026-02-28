329c6217eb0d8d6d7cc91213bdad4b05bd5552e0
bcedu <bcedu>
2026-02-28 13:36:21 +0100

    Servidor pos

--- SerPG/src/main.rs
+++ SerPG/src/main.rs
@@ -6,6 +6,8 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 use std::fs;
+use std::fs::File;
+use std::io::{Write, Read};
 use std::path::PathBuf;
 use axum::extract::State;
 use normalized_hash::Hasher;
@@ -23,6 +25,11 @@ struct PartidaGuardadaAPI {
     nom: String,
     hash: String
 }
+#[derive(Debug, Deserialize, Serialize)]
+struct PartidaGuardadaContingutAPI {
+    nom: String,
+    contingut: String
+}
 struct SerPG {
     pub router: Router,
 }
@@ -34,7 +41,8 @@ impl SerPG {
         let r = Router::new()
             .route("/api/v1/test", get(Self::test))
             .route("/api/v1/videojocs", get(Self::get_videojocs))
-            .route("/api/v1/videojocs/{videojoc_id}/partides", get(Self::get_partides_guardades))
+            .route("/api/v1/videojocs/{videojoc_id}/partides", get(Self::get_partides_guardades).post(Self::post_partida_guardada))
+            .route("/api/v1/videojocs/{videojoc_id}/partides/{partida_id}/contingut", get(Self::get_partida_guardada))
             .with_state(state)
         ;
         SerPG {router: r}
@@ -82,6 +90,25 @@ impl SerPG {
         }
         Json(partides_list)
     }
+    async fn get_partida_guardada(State(spg_state): State<SerPGState>, Path((videojoc_id, partida_id)): Path<(String, String)>) -> Json<PartidaGuardadaContingutAPI> {
+        let partida_path = format!("{}/{}/{}", spg_state.videojocs_path, videojoc_id, partida_id);
+        let mut contingut = String::new();
+        let mut f = File::open(partida_path).unwrap();
+        f.read_to_string(&mut contingut).unwrap();
+        drop(f);
+        Json(PartidaGuardadaContingutAPI{nom: partida_id, contingut})
+    }
+    async fn post_partida_guardada(State(spg_state): State<SerPGState>, Path(videojoc_id): Path<String>, Json(partida_nova): Json<PartidaGuardadaContingutAPI>) {
+        let partida_path = format!("{}/{}/{}", spg_state.videojocs_path, videojoc_id, partida_nova.nom.clone());
+        let videojoc_path = format!("{}/{}", spg_state.videojocs_path, videojoc_id);
+        if !PathBuf::from(&videojoc_path).exists() {
+            fs::create_dir(videojoc_path).unwrap();
+        }
+        let mut f = File::create(partida_path).unwrap();
+        f.write_all(partida_nova.contingut.as_bytes()).unwrap();
+        f.sync_all().unwrap();
+        drop(f);
+    }
 }
 #[tokio::main]
 async fn main() {
@@ -90,14 +117,16 @@ async fn main() {
 
 #[cfg(test)]
 pub mod tests {
-    use crate::SerPG;
+    use crate::{PartidaGuardadaContingutAPI, SerPG};
     use urlencoding::encode;
     use std::path::{PathBuf, Path};
+    use std::fs::{remove_dir_all, File};
+    use std::io::{Write, Read};
     use tokio::fs::remove_dir;
     async fn setup_server() {
         // Fem neteja decoses que no haurien de existir
         let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/pastanaga bullida").to_str().unwrap().to_string();
-        remove_dir(&test_path).await;
+        remove_dir_all(&test_path);
         // Fem el servidor
         let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures").to_str().unwrap().to_string();
         tokio::spawn(async {
@@ -110,7 +139,7 @@ pub mod tests {
         for endpoint_part in endpoint.split('/') {
             request_url = format!("{}/{}", request_url, encode(endpoint_part));
         }
-        println!("REQUEST: {}", request_url.clone());
+        println!("GET: {}", request_url.clone());
         let res = reqwest::Client::new()
             .get(request_url)
             .basic_auth("admin", Some("admin"))
@@ -120,6 +149,21 @@ pub mod tests {
         assert_eq!(res.status(), 200);
         let body = res.text().await.unwrap();
         body
+    }
+    async fn make_post_request(endpoint: &str, payload: PartidaGuardadaContingutAPI) {
+        let mut request_url = "http://127.0.0.1:3001/api/v1".to_string();
+        for endpoint_part in endpoint.split('/') {
+            request_url = format!("{}/{}", request_url, encode(endpoint_part));
+        }
+        println!("POST: {}", request_url.clone());
+        let res = reqwest::Client::new()
+            .post(request_url)
+            .basic_auth("admin", Some("admin"))
+            .json(&payload)
+            .send()
+            .await
+            .unwrap();
+        assert_eq!(res.status(), 200);
     }
     #[tokio::test]
     async fn test_api_get_test() {
@@ -150,5 +194,55 @@ pub mod tests {
         let res = make_get_request("videojocs/Napoleón TW HD/partides").await;
         let expected_res = "[{\"nom\":\"save1.txt\",\"hash\":\"02d47a22e09f46731a58dbe7cb299c0315c6760aec7557e8ca6e87090fc85dfd\"},{\"nom\":\"save3.txt\",\"hash\":\"158ed8c255d81393d423bc01c4993eceb3bb20a2596659ebc7f14ae82cbde4c8\"}]";
         assert_eq!(res, expected_res);
+    }
+    #[tokio::test]
+    async fn test_api_get_partida_guardada() {
+        let server = setup_server().await;
+        let res = make_get_request("videojocs/Napoleón TW HD/partides/save3.txt/contingut").await;
+        let expected_res = "{\"nom\":\"save3.txt\",\"contingut\":\"Soc una partida guardada del Total War 40k\\nPartida 3\"}";
+        assert_eq!(res, expected_res);
+    }
+    #[tokio::test]
+    async fn test_post_partida_guardada() {
+        let server = setup_server().await;
+        // Joc que no existeix
+        let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/pastanaga bullida").to_str().unwrap().to_string();
+        let test_partida_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/pastanaga bullida/save.txt").to_str().unwrap().to_string();
+        let partida_test = PartidaGuardadaContingutAPI {
+            nom: "save.txt".to_string(),
+            contingut: "@@".to_string(),
+        };
+        make_post_request("videojocs/pastanaga bullida/partides", partida_test).await;
+        let path = Path::new(&test_path);
+        assert!(Path::new(&path).exists());
+        let path = Path::new(&test_partida_path);
+        assert!(Path::new(&path).exists());
+        let mut contingut = String::new();
+        let mut f = File::open(test_partida_path.clone()).unwrap();
+        f.read_to_string(&mut contingut).unwrap();
+        drop(f);
+        assert_eq!(contingut, "@@");
+        // Joc amb partides
+        let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/Napoleón TW HD").to_str().unwrap().to_string();
+        let test_partida_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/Napoleón TW HD/save3.txt").to_str().unwrap().to_string();
+        let partida_test = PartidaGuardadaContingutAPI {
+            nom: "save3.txt".to_string(),
+            contingut: "Partida4".to_string(),
+        };
+        let original_content = "Soc una partida guardada del Total War 40k\nPartida 3";
+        make_post_request("videojocs/Napoleón TW HD/partides", partida_test).await;
+        let path = Path::new(&test_path);
+        assert!(Path::new(&path).exists());
+        let path = Path::new(&test_partida_path);
+        assert!(Path::new(&path).exists());
+        let mut contingut = String::new();
+        let mut f = File::open(test_partida_path.clone()).unwrap();
+        f.read_to_string(&mut contingut).unwrap();
+        drop(f);
+        assert_eq!(contingut, "Partida4");
+        let mut f = File::create(test_partida_path).unwrap();
+        f.write_all(original_content.as_bytes()).unwrap();
+        f.sync_all().unwrap();
+        drop(f)
     }
 }
