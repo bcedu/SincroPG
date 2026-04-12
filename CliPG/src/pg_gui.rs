4397a7952a0cf27f20bef0bbd1b1c371c0911154
bcedu <bcedu>
2026-04-12 10:52:03 +0200

    IMP UI

--- CliPG/src/pg_gui.rs
+++ CliPG/src/pg_gui.rs
@@ -1,6 +1,6 @@
 use crate::cli_pg::CliPG;
 use crate::videojoc::Videojoc;
-use eframe::egui;
+use eframe::egui::{self, CornerRadius, RichText};
 use rfd::FileDialog;
 use std::path::PathBuf;
 
@@ -41,6 +41,7 @@ impl Default for PgGUI {
     }
 }
 impl PgGUI {
+    // Metodes amb logica de aplicacio
     fn get_estat_servidor(&mut self) -> String {
         if self.estat_servidor.is_empty() {
             let clipg = CliPG::default(self.clipg_config_path.clone());
@@ -58,12 +59,12 @@ impl PgGUI {
     fn sincronitzar_tots(&mut self) {
         let mut clipg = CliPG::default(self.clipg_config_path.clone());
         let res = clipg.sync_all(false);
-        self.activitat = res;
+        self.activitat = res.trim().to_string();
     }
     fn sincronitzar_joc(&mut self, joc: &mut Videojoc) {
         let mut clipg = CliPG::default(self.clipg_config_path.clone());
         let res = clipg.sync_joc(joc, false);
-        self.activitat = res;
+        self.activitat = res.trim().to_string();
     }
     fn eliminar_joc(&mut self, joc: &mut Videojoc) {
         let mut clipg = CliPG::default(self.clipg_config_path.clone());
@@ -82,6 +83,49 @@ impl PgGUI {
         clipg.config.server.usuari = usuari;
         clipg.config.server.contrasenya = contrasenya;
         CliPG::save_config(&clipg.config, self.clipg_config_path.clone());
+    }
+}
+
+impl PgGUI {
+    // Metodes amb logica de GUI
+    fn ui_card<F: FnOnce(&mut egui::Ui)>(ui: &mut egui::Ui, title: Option<&str>, add: F) {
+        egui::Frame::group(ui.style()).corner_radius(CornerRadius::same(8)).inner_margin(12.0).show(ui, |ui| {
+            if let Some(title) = title {
+                ui.heading(RichText::new(title).strong());
+                ui.add_space(10.0);
+            }
+            add(ui);
+        });
+    }
+    fn ui_primary_button(ui: &mut egui::Ui, text: &str) -> egui::Response {
+        ui.add(
+            egui::Button::new(egui::RichText::new(text).color(egui::Color32::WHITE).strong().size(14.0))
+                .fill(egui::Color32::from_rgb(0, 120, 215))
+                .corner_radius(6.0),
+        )
+    }
+    fn ui_primary_danger_button(ui: &mut egui::Ui, text: &str) -> egui::Response {
+        ui.add(
+            egui::Button::new(egui::RichText::new(text).color(egui::Color32::WHITE).strong().size(14.0))
+                .fill(egui::Color32::from_rgb(230, 90, 90))
+                .corner_radius(6.0),
+        )
+    }
+    fn ui_secondary_button(ui: &mut egui::Ui, text: &str) -> egui::Response {
+        ui.add(
+            egui::Button::new(egui::RichText::new(text).color(egui::Color32::from_rgb(0, 120, 215)).strong())
+                .fill(egui::Color32::TRANSPARENT)
+                .stroke(egui::Stroke::new(1.0, egui::Color32::from_rgb(0, 120, 215)))
+                .corner_radius(6.0),
+        )
+    }
+    fn ui_secondary_danger_button(ui: &mut egui::Ui, text: &str) -> egui::Response {
+        ui.add(
+            egui::Button::new(egui::RichText::new(text).color(egui::Color32::from_rgb(230, 90, 90)).strong())
+                .fill(egui::Color32::TRANSPARENT)
+                .stroke(egui::Stroke::new(1.0, egui::Color32::from_rgb(230, 90, 90)))
+                .corner_radius(6.0),
+        )
     }
     fn setup_top_panel(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
         egui::menu::bar(ui, |ui| {
@@ -107,16 +151,14 @@ impl PgGUI {
     }
     fn setup_dashboard_videojocs_habilitats(&mut self, centered_ui: &mut egui::Ui) {
         centered_ui.add_space(10.0);
-        egui::Frame::group(centered_ui.style()).show(centered_ui, |group_ui| {
+        Self::ui_card(centered_ui, Some("🎮 Videojocs a sincronitzar"), |group_ui| {
             egui::ScrollArea::vertical().show(group_ui, |scroll_ui| {
-                scroll_ui.heading("🎮 Videojocs habilitats");
-                scroll_ui.add_space(10.0);
                 scroll_ui.horizontal(|row_ui| {
                     if row_ui.button("+ Afegir joc").clicked() {
                         self.current_mode = AppMode::EditarJoc;
                     }
                     row_ui.with_layout(egui::Layout::right_to_left(egui::Align::Min), |right_ui| {
-                        if right_ui.button("🔄 Sincronitzar tots").clicked() {
+                        if Self::ui_primary_button(right_ui, "🔄 Sincronitzar tots").clicked() {
                             self.sincronitzar_tots();
                         }
                     });
@@ -126,12 +168,12 @@ impl PgGUI {
                 clipg.load_local_jocs();
                 for joc in clipg.vjocs.iter_mut() {
                     scroll_ui.horizontal(|row_ui| {
-                        row_ui.label(joc.nom.clone().to_str().unwrap());
+                        row_ui.strong(joc.nom.clone().to_str().unwrap());
                         row_ui.with_layout(egui::Layout::right_to_left(egui::Align::Min), |right_ui| {
-                            if right_ui.button("🗑 Eliminar").clicked() {
+                            if Self::ui_secondary_danger_button(right_ui, "🗑").clicked() {
                                 self.eliminar_joc(joc);
                             }
-                            if right_ui.button("🔄 Sincronitzar").clicked() {
+                            if Self::ui_secondary_button(right_ui, "🔄").clicked() {
                                 self.sincronitzar_joc(joc);
                             }
                         });
@@ -143,16 +185,16 @@ impl PgGUI {
     }
     fn setup_dashboard_servidor_status(&mut self, centered_ui: &mut egui::Ui) {
         centered_ui.add_space(10.0);
-        egui::Frame::group(centered_ui.style()).show(centered_ui, |ui| {
+        Self::ui_card(centered_ui, None, |ui| {
             ui.horizontal(|ui| {
-                ui.label("Estat servidor:");
                 let estat_servidor = self.get_estat_servidor();
                 let color;
                 if estat_servidor.contains("Desconectat") {
-                    color = egui::Color32::RED;
+                    color = egui::Color32::DARK_RED;
                 } else {
-                    color = egui::Color32::GREEN;
+                    color = egui::Color32::DARK_GREEN;
                 }
+                ui.label("Estat servidor:");
                 ui.colored_label(color, format!("{}", estat_servidor));
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::Min), |ui| {
                     if ui.button("⚙ Editar").clicked() {
@@ -164,7 +206,7 @@ impl PgGUI {
     }
     fn setup_dashboard_activitat(&mut self, centered_ui: &mut egui::Ui) {
         centered_ui.add_space(10.0);
-        egui::Frame::group(centered_ui.style()).show(centered_ui, |group_ui| {
+        Self::ui_card(centered_ui, None, |group_ui| {
             group_ui.vertical_centered_justified(|vertical_ui| {
                 vertical_ui.vertical(|ui| {
                     egui::ScrollArea::vertical().auto_shrink([false, true]).show(ui, |scroll_ui| {
@@ -179,18 +221,19 @@ impl PgGUI {
     }
     fn setup_configuracio(&mut self, centered_ui: &mut egui::Ui) {
         centered_ui.add_space(10.0);
-        egui::Frame::group(centered_ui.style()).show(centered_ui, |group_ui| {
-            group_ui.heading("⚙ Configuració");
-            group_ui.add_space(10.0);
+        Self::ui_card(centered_ui, Some("⚙ Configuració"), |group_ui| {
             group_ui.vertical_centered_justified(|vui| {
+                vui.add_space(4.0);
                 vui.horizontal(|ui| {
                     ui.label("URL:");
                     ui.add(egui::TextEdit::singleline(&mut self.config_url).code_editor());
                 });
+                vui.add_space(4.0);
                 vui.horizontal(|ui| {
                     ui.label("Usuari:");
                     ui.add(egui::TextEdit::singleline(&mut self.config_usuari).code_editor());
                 });
+                vui.add_space(4.0);
                 vui.horizontal(|ui| {
                     ui.label("Contrasenya:");
                     ui.add(egui::TextEdit::singleline(&mut self.config_contrasenya).code_editor());
@@ -198,12 +241,12 @@ impl PgGUI {
                 vui.add_space(10.0);
                 vui.horizontal(|hui| {
                     hui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                        if ui.button("Desar").clicked() {
+                        if Self::ui_primary_button(ui, "Desar").clicked() {
                             self.guardar_configuracio(self.config_url.clone(), self.config_usuari.clone(), self.config_contrasenya.clone());
                             self.estat_servidor = String::new();
                             self.current_mode = AppMode::Dashboard;
                         }
-                        if ui.button("Cancel·lar").clicked() {
+                        if Self::ui_primary_danger_button(ui, "Cancel·lar").clicked() {
                             self.estat_servidor = String::new();
                             self.current_mode = AppMode::Dashboard;
                         }
@@ -215,10 +258,9 @@ impl PgGUI {
     }
     fn setup_editar_joc(&mut self, centered_ui: &mut egui::Ui) {
         centered_ui.add_space(10.0);
-        egui::Frame::group(centered_ui.style()).show(centered_ui, |group_ui| {
-            group_ui.heading("🎮 Afegir Joc");
-            group_ui.add_space(10.0);
+        Self::ui_card(centered_ui, Some("🎮 Afegir Joc"), |group_ui| {
             group_ui.vertical_centered_justified(|vui| {
+                vui.add_space(4.0);
                 vui.horizontal(|hui| {
                     hui.label("Directori de partides guardades:");
                     if self.joc_afegit.is_empty() {
@@ -234,6 +276,7 @@ impl PgGUI {
                         hui.add(egui::Label::new(&self.joc_afegit).wrap());
                     }
                 });
+                vui.add_space(4.0);
                 vui.horizontal_top(|ui| {
                     ui.label("Nom:");
                     ui.add(egui::TextEdit::singleline(&mut self.joc_afegit_nom));
@@ -241,12 +284,12 @@ impl PgGUI {
                 vui.add_space(10.0);
                 vui.horizontal(|hui| {
                     hui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                        if ui.button("Afegir").clicked() {
+                        if Self::ui_primary_button(ui, "Afegir").clicked() {
                             self.afegir_joc(self.joc_afegit.clone(), self.joc_afegit_nom.clone());
                             self.joc_afegit = String::new();
                             self.current_mode = AppMode::Dashboard;
                         }
-                        if ui.button("Cancel·lar").clicked() {
+                        if Self::ui_primary_danger_button(ui, "Cancel·lar").clicked() {
                             self.joc_afegit = String::new();
                             self.current_mode = AppMode::Dashboard;
                         }
@@ -256,9 +299,22 @@ impl PgGUI {
             group_ui.add_space(10.0);
         });
     }
+    fn setup_style(&mut self, ctx: &egui::Context) {
+        // Tema Clar/Fosc
+        if ctx.system_theme().or_else(|| Some(egui::Theme::Light)) == Some(egui::Theme::Light) {
+            ctx.set_theme(egui::Theme::Light);
+        } else {
+            ctx.set_theme(egui::Theme::Dark);
+        }
+        ctx.all_styles_mut(|style| {
+            // Marge als buttons
+            style.spacing.button_padding = egui::vec2(12.0, 4.0);
+        });
+    }
 }
 impl eframe::App for PgGUI {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.setup_style(ctx);
         egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
             self.setup_top_panel(ctx, ui);
         });
